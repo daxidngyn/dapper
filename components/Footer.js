373ea9c352583a0bade9f2d7034e6efe083347bc
Daev <daxidnguyen@gmail.com>
2021-07-30 23:46:54 -0700

    Responsive hero section

--- components/Footer.js
+++ components/Footer.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { FaBitcoin } from "react-icons/fa";
+import { FaEthereum } from "react-icons/fa";
 function Footer() {
+  const navigation = [
+    { name: "Marketplace", href: "#", current: false },
+    { name: "FAQ", href: "#", current: false },
+  ];
+
   return (
     <>
       <div className="pb-8 border-t-2">
-        <div className="mx-auto container pt-12 flex flex-col items-center justify-center">
+        <div className="mx-auto container pt-8 flex flex-col items-center justify-center">
           <div>
-            <FaBitcoin size={100} />
+            <FaEthereum size={64} />
           </div>
-          <div className="text-black flex flex-col md:items-center pt-3">
-            <h1 className="text-2xl font-black">Dapper</h1>
-            <div className="my-6 text-base">
-              <ul className="md:flex items-center pt-4">
-                <li className=" md:mr-6 cursor-pointer lg:py-0">GAY</li>
-                <li className=" md:mr-6 cursor-pointer lg:py-0">F</li>
-                <li className=" md:mr-6 cursor-pointer lg:py-0">DAVE</li>
-                <li className=" md:mr-6 cursor-pointer lg:py-0">BETA</li>
-                <li className=" md:mr-6 cursor-pointer lg:py-0">LEX</li>
-                <li className="cursor-pointer lg:py-0">SPED</li>
+          <div className="text-black flex flex-col md:items-center">
+            {/* <h1 className="text-2xl font-black">Dapper</h1> */}
+            <div className="my-6">
+              <ul className="md:flex items-center space-x-8">
+                {navigation.map((route) => (
+                  <li className="cursor-pointer lg:py-0">{route.name}</li>
+                ))}
               </ul>
             </div>
-            <div className="flex items-center mt-5 space-x-8 text-gray-500 font-light pb-4">
+            <div className="flex items-center space-x-8 text-gray-500 font-light pb-4">
               <h2 className="cursor-pointer">Terms of use</h2>
               <h2 className="cursor-pointer">Privacy policy</h2>
             </div>
