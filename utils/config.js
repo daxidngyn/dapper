0d346e833f27528941bdd80df5fad1e34b0e537b
Daev <daxidnguyen@gmail.com>
2021-08-15 03:00:54 -0700

    configured fetching dap

--- utils/config.js
+++ utils/config.js
@@ -1,9 +1,9 @@
 //testnet
 const config = {
-  CONTRACT_ADDR: "0x3095AeA37df854Cd530A7b714489B14c7691f6FA",
-  NET_ID: 80001,
-  CHAIN_ID: "0x13881",
-  CHAIN_NAME: "Matic Testnet Mumbai",
+  CONTRACT_ADDR: "0x06BCC7Ad62D18D174E7279837C7C782D239F6D10",
+  NET_ID: 4,
+  CHAIN_ID: "4",
+  CHAIN_NAME: "Rinkeby",
   RPC_URL:
     "https://rpc-mumbai.maticvigil.com/v1/a8f72913d3433046ab491c005256aec91a7a92b0",
   BLOCK_EXPLORER_URL: "https://explorer-mainnet.maticvigil.com/",
