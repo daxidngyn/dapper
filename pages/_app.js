7d1a5b7788b790bffc796706240b52e29e07bec9
Daev <daxidnguyen@gmail.com>
2021-08-15 13:46:50 -0700

    UI Changes

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,6 @@ import config from "../utils/config";
 
 import { BsFillExclamationCircleFill } from "react-icons/bs";
 
-// const config = {
-//   CONTRACT_ADDR: "0xdB66AcA61A75F38101b40c94155Fb1A1a872115c",
-//   NET_ID: 80001,
-//   CHAIN_ID: "0x13881",
-//   CHAIN_NAME: "Matic Testnet Mumbai",
-//   RPC_URL:
-//     "https://rpc-mumbai.maticvigil.com/v1/a8f72913d3433046ab491c005256aec91a7a92b0",
-//   BLOCK_EXPLORER_URL: "https://explorer-mainnet.maticvigil.com/",
-//   ABI_URL: "contract/Dapper.json",
-// };
-
 const Web3 = require("web3");
 var web3 = new Web3(Web3.givenProvider || config.RPC_URL);
 
--- pages/index.js
+++ pages/index.js
@@ -93,7 +93,7 @@ export default function Home(props) {
       <div className="bg-gradient-to-t from-blue-50 to-blue-300">
         <div className="pb-16 md:pt-24 sm:pt-12 pt-6 container max-w-7xl mx-auto flex flex-wrap flex-col items-center w-full text-gray-900 px-4 ">
           <div className="grid md:grid-cols-2 w-full grid-cols-1">
-            <div className="px-18 space-y-12 pt-2 md:order-1 order-2">
+            <div className="px-4 space-y-12 pt-2 md:order-1 order-2">
               <div className="space-y-1">
                 <div className="xl:text-7xl md:text-6xl sm:text-7xl text-5xl font-semibold">
                   Welcome to
